30964d5fc0341465c28e984efeb9b2c79483e5c8
Straker <jeferson.fg710@gmail.com>
2025-04-12 15:58:09 -0600

    feat: add check to ensure script runs as administrator

--- install-docker.ps1
+++ install-docker.ps1
@@ -1,3 +1,15 @@
+# Check if the script is running with admin privileges
+$Elevated = [System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent()
+$IsAdmin = $Elevated.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If not running as Administrator, restart the script as Administrator
+if (-not $IsAdmin) {
+    Write-Host "This script requires administrator privileges. Restarting as administrator..."
+    Start-Process powershell -ArgumentList "Start-Process powershell -ArgumentList '-File', '$PSCommandPath' -Verb RunAs" -Verb RunAs
+    exit
+}
+
+# If running as Administrator, proceed with the Docker installation
 Start-Process -Wait -FilePath "Docker Desktop Installer.exe" -ArgumentList `
   "install", `
   "-accept-license", `
